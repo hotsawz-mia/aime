0cb3465b817c0058afa0b419f8927f75567f58c7
jondedman <jonathandedman@googlemail.com>
2025-08-25 11:32:31 +0100

    set up middleware and wrap app in the the clerkprovider

---
+++ middleware.js
@@ -0,0 +1,29 @@
+// can add protected routes here as well
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// use the format below to create routes that are public and don't require authentication
+// currently this is set up so that users will be redirected to login sugn-up if they are not registered
+const isPublicRoute = createRouteMatcher([
+    "/",
+    "/login(.*)",
+    "register(.*)",
+//     "/book(.*)",
+])
+
+export default clerkMiddleware(async (auth, req) =>{
+    if (!isPublicRoute(req)) {
+        await auth.protect()        
+    }
+});
+
+// export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider>
+     <Component {...pageProps} />
+    </ClerkProvider>
+  )
 }
 
 export default MyApp
