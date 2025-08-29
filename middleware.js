1f5dfefc04efc6667189d2f20fce3323bc75d78f
jondedman <jonathandedman@googlemail.com>
2025-08-29 01:26:35 +0100

    update register route and remove empty component

--- middleware.js
+++ middleware.js
@@ -14,6 +14,7 @@ const isPublicRoute = createRouteMatcher([
     "/register(.*)",
     "/hello(.*)",
     "/api/dbtest",
+    "/results(.*)"
 ])
 
 export default clerkMiddleware(async (auth, req) =>{
