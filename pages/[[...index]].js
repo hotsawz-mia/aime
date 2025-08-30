bb4f8a60d6293059d05b5776610f04c94d027ccb
jondedman <jonathandedman@googlemail.com>
2025-08-30 17:25:23 +0100

    create sign in and sign up buttons and connections on home page and update clerk middle ware to protect the correct routes

---
+++ pages/[[...index]].js
@@ -0,0 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUp } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Home() {
+  return (<div>
+    <h2>Homepage - Welcome to Ai-me</h2>
+      <SignedIn>
+        <Link href="/form">Make a plan</Link>
+        <SignOutButton/>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>Sign In</SignInButton>
+        <SignUp path="/register">Sign Up</SignUp>
+      </SignedOut>
+    </div>
+  )
+}
