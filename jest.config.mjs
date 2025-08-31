079882318bab9b8cb01ec202bcfbe62c5021c0dc
jondedman <jonathandedman@googlemail.com>
2025-08-31 22:46:34 +0100

    tidy up

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,15 +1,3 @@
-// export default {
-//   testEnvironment: "node",
-//   transform: {}, // no Babel
-//   moduleFileExtensions: ["js", "jsx", "mjs", "cjs", "json"],
-// };
-
-// export default {
-//     testEnvironment: "node",
-//     transform: {},
-//   };
-
-// jest.config.mjs
 export default {
   testEnvironment: "jsdom",
   extensionsToTreatAsEsm: [".jsx"], // Remove ".mjs"
