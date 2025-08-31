0364b2b5f8f1396260218e7f99b4e429f7ef02a1
jondedman <jonathandedman@googlemail.com>
2025-08-31 16:34:26 +0100

    create a simple practice test

---
+++ tests/pages/authflow.test.js
@@ -0,0 +1,23 @@
+
+
+
+
+describe('This is a simple practice test suite', ()=>{
+    test('checks a calculation', ()=>{
+        const result = 2 + 2;
+        expect(result).toBe(4);
+    })
+})
+
+// test that a logged in user gets redirected is show a page with
+// signout button/link
+
+// tests that a logged out user is presented with a signin 
+// and signup link or button
+
+// tests that the home page has a link to the formpage
+
+// tests that the the link navigates to the form page when logged in
+// tests that the form page is protected when not logged in
+// tests that the signout button returns the user to the home page
+// from the planid page
