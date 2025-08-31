f2c7f893b65b5f4239273dfa547a5683af45ab93
jondedman <jonathandedman@googlemail.com>
2025-08-31 19:15:05 +0100

    expand clerk mocks

--- tests/mocks/clerk.js
+++ tests/mocks/clerk.js
@@ -10,3 +10,11 @@ export const useClerk = jest.fn(() => ({
 }));
 
 export const withAuth = (Component) => Component;
+
+export const SignedIn = ({ children }) => <>{children}</>;
+export const SignedOut = ({ children }) => <>{children}</>;
+export const SignInButton = ({ children }) => <button>{children || "Sign In"}</button>;
+export const SignOutButton = ({ children }) => <button>{children || "Sign Out"}</button>;
+export const SignUp = ({ children }) => <button>{children || "Sign Up"}</button>;
+
+
