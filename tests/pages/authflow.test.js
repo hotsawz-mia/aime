8f3d6227f99f0b51662d10c9d747f2c1356381ea
jondedman <jonathandedman@googlemail.com>
2025-09-01 12:55:07 +0100

    add tests for Link to form from home page

--- tests/pages/authflow.test.js
+++ tests/pages/authflow.test.js
@@ -1,7 +1,24 @@
 import React from "react";
 import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useRouter } from "next/router";
+
+// Mock the router - notice this has to happen before importing the component
+// as the component imports these modules.
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+jest.mock("next/link", () => {
+  return ({ href, children }) => (
+    <button onClick={() => require("next/router").useRouter().push(href)}>
+      {children}
+    </button>
+  );
+});
 import Home from "../../pages/[[...index]]";
+
 
 describe('This is a simple practice test suite', ()=>{
     test('checks a calculation', ()=>{
@@ -17,7 +34,7 @@ describe('signed in and signed out', ()=>{
         expect(screen.getByText(/sign in/i)).toBeInTheDocument();
         expect(screen.getByText(/sign up/i)).toBeInTheDocument();        
     })
-    test('// test that a logged in user gets redirected to show a page with', () =>{
+    test('test that a logged in user gets redirected to show a page with sign and and make a plan', () =>{
         const { useUser } = require("@clerk/nextjs");
         useUser.mockReturnValue({
         isSignedIn: true,
@@ -26,11 +43,29 @@ describe('signed in and signed out', ()=>{
         render(<Home/>)
         expect(screen.getByText(/sign out/i)).toBeInTheDocument();
         expect(screen.getByText(/Make a plan/i)).toBeInTheDocument();
+        expect(screen.getByText(/Make a plan/i)).toBeInTheDocument();
     })
+
+      test('test that a make a plan link navigates to the form page when clicked', async () => {
+    const { useUser } = require("@clerk/nextjs");
+    useUser.mockReturnValue({
+      isSignedIn: true,
+      user: { id: "user_123", email: "test@example.com" },
+    });
+
+    // Mock the router
+    const push = jest.fn();
+    jest.spyOn(require("next/router"), "useRouter").mockReturnValue({ push });
+
+    render(<Home />);
+    const makeAPlanLink = screen.getByText(/Make a plan/i);
+    await userEvent.click(makeAPlanLink);
+
+    expect(push).toHaveBeenCalledWith("/form");
+  });
 })
 
-// test that a logged in user gets redirected to show a page with
-// signout button/link
+// 
 
 // tests that a logged out user is presented with a signin 
 // and signup link or button
