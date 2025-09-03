64d07ef091f49ae8857fb88c6cb60a76b346e86a
jondedman <jonathandedman@googlemail.com>
2025-09-03 22:38:00 +0100

    remove next.js dev indicator

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    devIndicators: {
+    buildActivity: false,        // Disables the build activity indicator
+  },
 }
 
 module.exports = nextConfig
