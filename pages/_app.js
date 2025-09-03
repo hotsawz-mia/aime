e4607522e00f15b4cbd8c95034777788cbec1741
jondedman <jonathandedman@googlemail.com>
2025-09-03 22:14:23 +0100

    center login div and apply synthwave theme globally

--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ClerkProvider>
-      <div data-theme="nord">
+      <div data-theme="synthwave">
         {!isAuthPage && shouldShowNavbar && <Navbar />}
         <Component {...pageProps} />
       </div>
--- pages/login/[[...index]].jsx
+++ pages/login/[[...index]].jsx
@@ -3,7 +3,11 @@ import { SignIn } from "@clerk/nextjs";
 export default function LoginPage() {
     // note because of the catch all route used this works. if you want to use this outside of catchall route, you need
     //  to pass it routing =hash. You can also add an appearance prop from clerk for styling iof you want.
-    return <SignIn
-    // routing="hash"
-    />;
+    return (
+        <div className="min-h-screen bg-base-200">
+            <div className="container mx-auto flex items-center justify-center min-h-screen">
+                <SignIn />
+            </div>
+        </div>
+    )
 }
