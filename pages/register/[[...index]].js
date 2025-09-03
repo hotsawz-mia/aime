d9c120aea1246f3bb06f64936d47162360b9a5e5
Alianna Bonifacio <aliannabonifacio@MASTUDENT-Alianna-Bonifacio-C17GMB1QQ6L4.local>
2025-09-03 17:23:08 +0100

    create styling for login/registration

--- pages/register/[[...index]].js
+++ pages/register/[[...index]].js
@@ -1,12 +1,19 @@
 import { SignUp } from "@clerk/nextjs";
+import {dark, shadesOfPurple} from "@clerk/themes";
+
 
 export default function RegisterPage() {
     // note because of the catch all route used this works. if you want to use this outside of catchall route, you need
     //  to pass it routing =hash. You can also add an appearance prop from clerk for styling iof you want.
     return (
-    <div className="bg-red-500">
+        
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
     <SignUp
     // routing="hash"
+    appearance={{
+            baseTheme:shadesOfPurple,
+                variables: { colorPrimary: 'oklch(76% 0.233 130.85)' },
+            }}
     />
 </div>);
 }
